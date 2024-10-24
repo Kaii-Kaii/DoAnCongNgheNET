a55e2d4b6195e7ce998cdba65a55fed0fd2f73e6
keve23z <nguyenvuonghongdao2004@gmail.com>
2024-10-24 13:09:19 +0700

    ad

--- UNG_DUNG_QUAN_LY_XE_GAN_MAY/Login.Designer.cs
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/Login.Designer.cs
@@ -147,7 +147,7 @@
             // 
             // txt_Pass
             // 
-            this.txt_Pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Pass.Location = new System.Drawing.Point(392, 260);
             this.txt_Pass.Name = "txt_Pass";
             this.txt_Pass.Size = new System.Drawing.Size(258, 30);
--- UNG_DUNG_QUAN_LY_XE_GAN_MAY/Program.cs
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/Program.cs
@@ -17,7 +17,7 @@ namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frm_LoadApp());
-            Application.Run(new frm_AdminApp());
+            Application.Run(new frm_Login());
         }
     }
 }
