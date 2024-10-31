e5af368fd977467b1b72912936977bc3320728e7
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-10-31 15:20:57 +0700

    update login

--- UNG_DUNG_QUAN_LY_XE_GAN_MAY/Login.cs
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/Login.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography.Xml;
 
 namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY
 {
+
+    
     public partial class frm_Login : Form
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ConnectionString);
         public frm_Login()
         {
             InitializeComponent();
@@ -42,6 +48,51 @@ namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY
         {
             if (chb_Show.Checked) txt_Pass.PasswordChar = '\0';
             else txt_Pass.PasswordChar = '*';
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            //CREATE TABLE TAIKHOAN_NV (
+            //MA_NV CHAR(6) PRIMARY KEY,  --Mã nhân viên(khóa ngoại từ bảng NHANVIEN)
+            //PASS NVARCHAR(50) NOT NULL,  --Mật khẩu
+            //FOREIGN KEY(MA_NV) REFERENCES NHANVIEN(MA_NV)  --Khóa ngoại tham chiếu đến bảng NHANVIEN
+            //);
+            // kiểm tra dữ liệu nhập vào
+            if (txt_TDN.Text == "" || txt_Pass.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            // kiểm tra tài khoản và mật khẩu
+            conn.Open();
+            string query = "SELECT COUNT(*) FROM TAIKHOAN_NV WHERE MA_NV = @MANV AND PASS = @PASS";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@MANV", txt_TDN.Text);
+            cmd.Parameters.AddWithValue("@PASS", txt_Pass.Text);
+            int result = (int)cmd.ExecuteScalar();
+            conn.Close();
+            if (result > 0)
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (txt_TDN.Text == "NV001")
+                {
+                    frm_AdminApp frm = new frm_AdminApp();
+                    this.Hide();
+                    frm.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    //frm_MainNV frm = new frm_MainNV(txt_TDN.Text);
+                    //this.Hide();
+                    //frm.ShowDialog();
+                    //this.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
         // hello nhen
         // ụaaaa
--- UNG_DUNG_QUAN_LY_XE_GAN_MAY/Properties/Settings.Designer.cs
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/Properties/Settings.Designer.cs
@@ -8,22 +8,40 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY.Properties
-{
-
-
+namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KAIIKAII;Initial Catalog=QL_CH_Xe;Integrated Security=True;TrustServe" +
+            "rCertificate=True")]
+        public string QL_CH_XeConnectionString {
+            get {
+                return ((string)(this["QL_CH_XeConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KAIIKAII;Initial Catalog=QL_CH_Xe;Integrated Security=True;TrustServe" +
+            "rCertificate=True")]
+        public string QL_CH_XeConnection {
+            get {
+                return ((string)(this["QL_CH_XeConnection"]));
             }
         }
     }
