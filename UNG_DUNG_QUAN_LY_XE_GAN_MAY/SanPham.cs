5fd54d96800199c98238049d3713bd4330c947a6
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-11-21 14:21:48 +0700

    Create SanPham.cs

---
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/SanPham.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY
+{
+    public class SanPham
+    {
+        public string MaSP { get; set; }
+        public string TenSP { get; set;}
+        public string MoTa { get; set; }
+        public int SoLuong { get; set; }
+        public decimal GiaBan { get; set; }
+        public decimal GiaNhap { get; set; }
+        public int TgBaoHanh { get; set; }
+        public string AnhSP { get; set; }
+        public string MaLoai { get; set; }
+
+    }
+}
