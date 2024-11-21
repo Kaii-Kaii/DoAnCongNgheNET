54920d89086a61b36cca92493e20e153ba66cab0
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-11-21 14:21:20 +0700

    Create KhachHang.cs

---
+++ UNG_DUNG_QUAN_LY_XE_GAN_MAY/KhachHang.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNG_DUNG_QUAN_LY_XE_GAN_MAY
+{
+    public class KhachHang
+    {
+        public string SDTKH { get; set; }
+        public string TenKH { get; set; }
+        public string DiaChi { get; set; }
+        public string Mail { get; set; }
+        
+    }
+}
